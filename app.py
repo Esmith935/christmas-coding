e3221ced5e811b059a5b9d0514ceaec9a1b8cf79
Toby-A-eng <tobyroyanderson@gmail.com>
2025-12-17 09:43:59 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,13 @@ import os
 
 
 app = Flask(__name__)
+
+# -- Route: Index
+
+@app.route('/')
+def index():
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug = True)
